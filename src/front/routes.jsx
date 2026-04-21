2ae1976302d3f6f32cf86cb6fb1cc14ddab56c5f
jcasib <jcasib01@gmail.com>
2026-04-21 14:15:07 +0000

    routes modified

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -7,8 +7,6 @@ import {
 } from "react-router-dom";
 import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
-import { Single } from "./pages/Single";
-import { Demo } from "./pages/Demo";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,8 +21,6 @@ export const router = createBrowserRouter(
 
         {/* Nested Routes: Defines sub-routes within the BaseHome component. */}
         <Route path= "/" element={<Home />} />
-        <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
-        <Route path="/demo" element={<Demo />} />
       </Route>
     )
 );
