af4a7ae1cd0851ed6c10f7fd23f63ef7d7381c3b
jcasib <jcasib01@gmail.com>
2026-04-21 13:59:49 +0000

    app.py modified

--- src/app.py
+++ src/app.py
@@ -5,11 +5,16 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager
+from api.extensions import limiter
+from datetime import timedelta
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from api.scheduler import start_scheduler, scheduler
+import atexit
 
 # from models import Person
 
@@ -18,6 +23,13 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+# Configuración del hash en password
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
+jwt = JWTManager(app)
+
+# Configuración anti fuerza bruta
+limiter.init_app(app)
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -30,6 +42,11 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
+
+with app.app_context():
+    start_scheduler()
+
+atexit.register(lambda: scheduler.shutdown())
 
 # add the admin
 setup_admin(app)
@@ -57,6 +74,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
