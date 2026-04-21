4d1acfd5579eb12a8eabffe911cab4e10b0c7cd8
jcasib <jcasib01@gmail.com>
2026-04-21 14:17:28 +0000

    layout modified

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,14 +2,20 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import { MobileNavbar } from "../components/Navbar/MobileNavbar"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
-            <Navbar />
+            {/* <Navbar /> */}
+            <div className="flex-grow-1 overflow-auto p-3 pb-5 pb-lg-2 mb-5">
                 <Outlet />
-            <Footer />
+            </div>
+            <div className="d-lg-none">
+                <MobileNavbar />
+            </div>
+            {/* <Footer /> */}
         </ScrollToTop>
     )
 }
