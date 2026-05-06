586d74f8a694f72e7978d2547bdde9bd23200963
jcasib <jcasib01@gmail.com>
2026-05-06 16:19:47 +0000

    Privacy for auth

--- src/api/routes.py
+++ src/api/routes.py
@@ -90,7 +90,7 @@ def signup():
     if not re.match(password_regex, password):
         return jsonify({"error": "The password must contain at least one letter and one number"}), 400
 
-    new_user = User(email=email, username=username)
+    new_user = User(email=email, username=username, accepted_privacy=True)
     new_user.set_password(password)
 
     try:
